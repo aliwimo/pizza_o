829a2466602f7f2bb329c67e0d0dda27cc86e851
aliwimo <aliwimo@gmail.com>
2021-06-28 04:03:00 +0300

    renamed controller

---
+++ pizza_system/app/Http/Controllers/IngredientsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+use ingredients;
+
+class IngredientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ingredients = Ingredient::all();
+        return view('ingredient.index')->with('ingredients', $ingredients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('ingredient.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $ingredient = Ingredient::create([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+        ]);
+
+        return  redirect('/ingredient');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // $ingredient = Ingredient::where('id', $id)->first();
+        $ingredient = Ingredient::find($id);
+        return view('ingredient.edit')->with('ingredient', $ingredient);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ingredient = Ingredient::where('id', $id)->update([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+        ]);
+
+
+        return redirect('/ingredient');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Ingredient::where('id', $id)->first()->delete();
+        return redirect('/ingredient');
+    }
+}
